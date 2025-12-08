e8072add1a99964a4f901b9e7db7a9414aacb00f
StefanosVegkos <st.vengos@gmail.com>
2025-12-08 20:36:25 +0200

    Answer to Q1

--- 2025/Q1.py
+++ 2025/Q1.py
@@ -1,1 +1,9 @@
-print("still testing")
+zeroesfound = 0
+location = 50
+with open("Q1 inputs.txt",) as file:
+    for line in file:
+        amount = int(line.strip()[1:])
+        multiplier = 1 if line[0] == "R" else -1
+        location = (location + (amount * multiplier)) % 100
+        zeroesfound += 1 if location == 0 else 0
+print("Solution to Q1: ", zeroesfound)
